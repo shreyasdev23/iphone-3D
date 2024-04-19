3bef26b3bab437147272ec7b40901f84ff6f9d40
shreyasdev23 <shreyasm670@gmail.com>
2024-04-19 18:40:29 +0530

    firsst commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+
+import * as Sentry from '@sentry/react';
+import Features from "./components/Features";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+
+    </main>
+  );
+};
+
+export default Sentry.withProfiler(App);
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e288a52751c772810237bf2185cc7bac@o4507083207999488.ingest.us.sentry.io/4507083230806016",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect : React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
